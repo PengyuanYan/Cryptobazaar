ca5c016178a3e1844466c7a33c073cfedf366ab7
Pengyuan Yan <pengyuanyan@PengyuandeMacBook-Pro.local>
2025-06-29 21:35:16 +0100

    Reimplement unsafe_setup_from_tau based on the original Cryptobazaar implementation

---
+++ src/utils/srs.rs
@@ -0,0 +1,34 @@
+use icicle_core::curve::{Curve, Affine, Projective};
+use icicle_core::traits::FieldImpl;
+use std::ops::Mul;
+
+pub fn unsafe_setup_from_tau<C>(
+     max_power: usize,
+     tau: C::ScalarField,
+) -> Vec<Affine<C>>
+where
+    C: Curve,
+    C::ScalarField: Mul<Output = C::ScalarField>,
+{
+    let size = max_power + 1;
+
+    let mut scalars = Vec::with_capacity(size);
+    let mut acc = C::ScalarField::from_u32(1u32);
+
+    for _ in 0..size {
+        scalars.push(acc);
+        acc = acc * tau;
+    }
+    
+    let projective_g1: Projective<C> = C::get_generator();
+    
+    let mut srs = Vec::with_capacity(size);
+    for i in &scalars {
+        let projective_base = C::mul_scalar(projective_g1, *i);
+        let mut affine_base = Affine::<C>::zero();
+        C::to_affine(&projective_base, &mut affine_base);
+        srs.push(affine_base);
+    }
+
+    srs
+}
