c4cb2e014803e411860c3bfe69f2026646ca459f
Pengyuan Yan <pengyuanyan@PengyuandeMacBook-Pro.local>
2025-07-01 22:56:33 +0100

    Reimplement the VK struct, open, and verify functions based on the original Cryptobazaar implementation

--- src/kzg.rs
+++ src/kzg.rs
@@ -9,6 +9,13 @@ use icicle_runtime::memory::HostSlice;
 use icicle_core::polynomials::UnivariatePolynomial;
 use crate::utils::get_coeffs_of_poly;
 
+use std::ops::{Add, Mul, Sub};
+
+#[derive(Debug)]
+pub enum Error {
+    PairingNot0,
+}
+
 pub struct Kzg<C1, C2, F>
 where
     C1: Curve,
@@ -37,9 +44,25 @@ where
     F: FieldImpl,
     C1: Pairing<C1, C2, F>,
 {
-    pub g2: C2,
-    pub neg_x_g2: C2,
+    pub g2: Affine<C2>,
+    pub x_g2: Affine<C2>,
     _e: PhantomData<(C1, F)>,
+}
+
+impl<C1, C2, F> VK<C1, C2, F>
+where
+    C1: Curve,
+    C2: Curve,
+    F: FieldImpl,
+    C1: Pairing<C1, C2, F>,
+{
+    pub fn new(x_g2: Affine<C2>) -> Self {
+        Self {
+            g2: C2::get_generator().into(),
+            x_g2,
+            _e: PhantomData,
+        }
+    }
 }
 
 impl<C1, C2, F> Kzg<C1, C2, F>
@@ -81,6 +104,106 @@ where
         C1::to_affine(&projective_output[0], &mut affine_output);
         affine_output
     }
+
+    pub fn open<P>(
+        pk: &PK<C1, C2, F>,
+        polys: &[P],
+        opening_challenge: C1::ScalarField,
+        separation_challenge: C1::ScalarField,
+    ) -> Affine<C1>
+    where
+        P: UnivariatePolynomial<Field = C1::ScalarField>,
+        for<'a> &'a P: std::ops::Add<&'a P, Output = P>,
+        C1::ScalarField: Mul<Output = C1::ScalarField>,
+        C1::ScalarField: Sub<Output = C1::ScalarField>,
+    {
+        let powers_of_gamma = std::iter::successors(Some(separation_challenge), |p| {
+            Some(*p * separation_challenge)
+        });
+
+        let mut batched = polys[0].clone();
+        for (p_i, gamma_pow_i) in polys.iter().skip(1).zip(powers_of_gamma) {
+            let gamma_poly = p_i.mul_by_scalar(&gamma_pow_i);
+            batched = &gamma_poly + &batched;
+        }
+        
+        let coeffs = [C1::ScalarField::zero() - opening_challenge, C1::ScalarField::one()];
+        let divisor_poly = P::from_coeffs(HostSlice::from_slice(&coeffs), 2);
+
+        let (q, _) = batched.divide(&divisor_poly);
+
+        if pk.srs.len() - 1 < q.degree().try_into().unwrap() {
+            panic!(
+                "Batch open g1: SRS size to small! Can't commit to polynomial of degree {} with srs of size {}",
+                q.degree(),
+                pk.srs.len()
+            );
+        }
+
+        Kzg::commit(pk, &q)
+    }
+
+    pub fn verify(
+        commitments: &[Affine<C1>],
+        evaluations: &[C1::ScalarField],
+        opening_proof: Affine<C1>,
+        opening_challenge: C1::ScalarField,
+        separation_challenge: C1::ScalarField,
+        vk: &VK<C1, C2, F>,
+    ) -> Result<(), Error>
+    where
+        C1::ScalarField: Mul<Output = C1::ScalarField>,
+        C1::ScalarField: Add<Output = C1::ScalarField>,
+        C1::ScalarField: Sub<Output = C1::ScalarField>,
+        //F: std::ops::Mul,
+    {
+        assert_eq!(commitments.len(), evaluations.len());
+        let powers_of_gamma: Vec<_> = std::iter::successors(Some(C1::ScalarField::one()), |p| {
+            Some(*p * separation_challenge)
+        })
+        .take(commitments.len())
+        .collect();
+        
+        let mut cfg = MSMConfig::default();
+        let mut batched_commitment = vec![Projective::<C1>::zero(); 1];
+        msm::msm(
+            HostSlice::from_slice(&powers_of_gamma),
+            HostSlice::from_slice(commitments),
+            &cfg,
+            HostSlice::from_mut_slice(&mut batched_commitment),
+        )
+        .unwrap();
+        
+        let batched_eval: C1::ScalarField = evaluations
+            .iter()
+            .zip(powers_of_gamma.iter())
+            .map(|(&ei, &gamma_i)| ei * gamma_i)
+            .fold(C1::ScalarField::zero(), |acc, x| acc + x);
+        
+        /*
+            (p(X) - y) = q(X)(X - z)
+            p(X) - y = q(X)•X - q(X)z
+            p(X) - y + q(X)z = q(X)•X
+            e([p] + z[q] - y[1], [1]) = e([q], [x])
+            e([p] + z[q] - y[1], [1])*e([q], -[x]) = 0
+        */
+        
+        let opening = opening_proof.to_projective() * opening_challenge;
+        let minus_eval = C1::ScalarField::zero() - batched_eval;
+        let y_root = C1::get_generator() * minus_eval;
+
+        let lhs = batched_commitment[0] + opening + y_root;
+        let lhs_affine: Affine<C1> = lhs.into();
+        
+        let e1: F = C1::pairing(&lhs_affine, &vk.g2).expect("pairing failed");
+        let e2: F = C1::pairing(&lhs_affine, &vk.g2).expect("pairing failed");
+
+        if e1 != e2 {
+            return Err(Error::PairingNot0);
+        }
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -92,7 +215,7 @@ mod test_kzg {
     use icicle_core::polynomials::UnivariatePolynomial;
     use icicle_runtime::memory::HostSlice;
     
-    use icicle_bn254::curve::{CurveCfg as Bn254CurveCfg, G2CurveCfg as Bn254G2CurveCfg, G1Projective, G1Affine};
+    use icicle_bn254::curve::{CurveCfg as Bn254CurveCfg, G2CurveCfg as Bn254G2CurveCfg, G1Projective, G1Affine, G2Projective};
     use icicle_bn254::pairing::PairingTargetField as Bn254PairingFieldImpl;
     use icicle_core::curve::Curve;
 
@@ -120,6 +243,57 @@ mod test_kzg {
         let mut output = vec![G1Projective::zero(); size];
         
         let commit = Kzg::commit(&pk, &poly);
+
+        let z = Bn254ScalarField::from_u32(10u32);
+        let gamma = Bn254ScalarField::from_u32(20u32);
+        
+        let polys = [poly.clone()];
+        let q = Kzg::open(&pk, &polys, z, gamma);
+
+        let eval = poly.eval(&z);
+        
+        let x_g2 = Bn254G2CurveCfg::get_generator() * tau;
+
+        let vk = VK::<Bn254CurveCfg, Bn254G2CurveCfg, Bn254PairingFieldImpl>::new(x_g2.into());
+
+        let verify_result = Kzg::verify(&[commit], &[eval], q, z, gamma, &vk);
+        assert!(verify_result.is_ok());
+    }
+    
+    #[test]
+    fn test_batched_kzg_real() {
+        let size = 16;
+        let a_coeffs = ScalarCfg::generate_random(size);
+        let a_poly = Bn254DensePolynomial::from_coeffs(HostSlice::from_slice(&a_coeffs), size);
+
+        let b_coeffs = ScalarCfg::generate_random(size);
+        let b_poly = Bn254DensePolynomial::from_coeffs(HostSlice::from_slice(&b_coeffs), size);
+
+        let tau = Bn254ScalarField::from_u32(100u32);
+        let srs = unsafe_setup_from_tau::<Bn254CurveCfg>(size - 1, tau);
+
+        let x_g2 = Bn254G2CurveCfg::get_generator() * tau;
+        let vk = VK::<Bn254CurveCfg, Bn254G2CurveCfg, Bn254PairingFieldImpl>::new(x_g2.into());
+        
+        let pk = PK::<Bn254CurveCfg, Bn254G2CurveCfg, Bn254PairingFieldImpl> { srs: srs.clone(), _e: PhantomData, };
+
+        let mut cfg = MSMConfig::default();
+        let mut output = vec![G1Projective::zero(); size];
+        
+        let a_commit = Kzg::commit(&pk, &a_poly);
+        let b_commit = Kzg::commit(&pk, &b_poly);
+
+        let z = Bn254ScalarField::from_u32(10u32);
+        let gamma = Bn254ScalarField::from_u32(20u32);
+        
+        let polys = [a_poly.clone(), b_poly.clone()];
+        let q = Kzg::open(&pk, &polys, z, gamma);
+
+        let a_eval = a_poly.eval(&z);
+        let b_eval = b_poly.eval(&z);
+
+        let verify_result = Kzg::verify(&[a_commit, b_commit], &[a_eval, b_eval], q, z, gamma, &vk);
+        assert!(verify_result.is_ok());
     }
 
     #[test]
