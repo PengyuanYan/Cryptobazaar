d6c76e5f1c8f37f222f44627cb33b45c848b878b
Pengyuan Yan <pengyuanyan@PengyuandeMacBook-Pro.local>
2025-07-20 22:00:48 +0100

    Update lib

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,6 @@ pub mod bid_encoder;
 pub mod bidder;
 pub mod zk_log_derivative;
 pub mod verifiable_folding_sumcheck;
+pub mod univariate_sumcheck;
+pub mod pedersen_schnorr;
+pub mod double_pedersen_schnorr;
