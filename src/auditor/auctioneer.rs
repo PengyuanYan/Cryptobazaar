6a424fc1a4b9dce3d343f1a059de048e77486c6b
Pengyuan Yan <pengyuanyan@PengyuandeMacBook-Pro.local>
2025-08-04 03:29:55 +0100

    Add dedicated Error type for auditor

--- src/auditor/auctioneer.rs
+++ src/auditor/auctioneer.rs
@@ -4,6 +4,13 @@ use icicle_runtime::memory::HostSlice;
 use icicle_core::traits::FieldImpl;
 use icicle_core::traits::Arithmetic;
 
+#[derive(Debug)]
+pub enum Error {
+    FirstRoundAuditFail,
+    SecondRoundAuditFail,
+    AuditFail { bid_at: usize, error_detail: Box<Error> },
+}
+
 pub struct AVAuditor<const B: usize, C: Curve + icicle_core::msm::MSM<C>> {
     pub(crate) first_round_msgs: [Affine::<C>; B],
     pub(crate) first_round_output: [Affine::<C>; B],
@@ -12,12 +19,15 @@ pub struct AVAuditor<const B: usize, C: Curve + icicle_core::msm::MSM<C>> {
 }
 
 impl<const B: usize, C: Curve + icicle_core::msm::MSM<C>> AVAuditor<B, C> {
-    pub fn audit(&self) -> bool 
+    pub fn audit(&self) -> Result<(), Error> 
     where
         C: Curve<ScalarField: Arithmetic>
-    {
-        let mut x = [C::ScalarField::zero() - C::ScalarField::one(); B];
-        x[0] = C::ScalarField::zero();
+    { 
+        // x = [0, -1, -1, ..., -1]
+        let x: Vec<C::ScalarField> = (0..B)
+            .map(|i| if i == 0 { C::ScalarField::one() }
+                     else { C::ScalarField::zero() - C::ScalarField::one() })
+        .collect();
 
         let mut outputs = vec![Affine::<C>::zero(); B];
 
@@ -50,7 +60,11 @@ impl<const B: usize, C: Curve + icicle_core::msm::MSM<C>> AVAuditor<B, C> {
             outputs[i] = affine_output;
         }
 
-        let first_round_res = outputs;
+        let first_round_result = outputs;
+
+        if first_round_result != self.first_round_output {
+            return Err(Error::FirstRoundAuditFail);
+        }
 
         let ones = vec![C::ScalarField::one(); B];
         
@@ -68,10 +82,13 @@ impl<const B: usize, C: Curve + icicle_core::msm::MSM<C>> AVAuditor<B, C> {
         let mut affine_output = Affine::<C>::zero();
         C::to_affine(&projective_output[0], &mut affine_output);
 
-        let second_round_res = affine_output;
+        let second_round_result = affine_output;
 
-        return first_round_res == self.first_round_output
-            && second_round_res == self.second_round_output;
+        if second_round_result != self.second_round_output {
+            return Err(Error::SecondRoundAuditFail);
+        }
+
+        Ok(())
     }
 }
 
@@ -80,13 +97,22 @@ pub struct AuctioneerAuditor<const N: usize, const B: usize, C: Curve  + icicle_
 }
 
 impl<const N: usize, const B: usize, C: Curve + icicle_core::msm::MSM<C>> AuctioneerAuditor<N, B, C> {
-    pub fn audit(&self) -> bool where C: Curve<ScalarField: Arithmetic> {
+    pub fn audit(&self) -> Result<(), Error> 
+    where
+        C: Curve<ScalarField: Arithmetic>
+    {
         for i in 0..N {
-            if !self.av_auditor[i].audit() {
-                return false;
+            match self.av_auditor[i].audit() {
+                Ok(()) => continue,
+                Err(inner_error) => {
+                    return Err(Error::AuditFail {
+                        bid_at: i,
+                        error_detail: Box::new(inner_error),
+                    });
+                }
             }
         }
 
-        return true;
+        Ok(())
     }
 }
