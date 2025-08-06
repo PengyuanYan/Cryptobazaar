fb8b2fee12afe6e9034688646e8f7c9623ce14c6
Pengyuan Yan <pengyuanyan@PengyuandeMacBook-Pro.local>
2025-08-06 19:17:55 +0100

    Add a benchmark for msm

--- benches/msmbenchmark.rs
+++ benches/msmbenchmark.rs
@@ -1,10 +1,10 @@
 use icicle_core::{msm, msm::MSMConfig};
-use icicle_runtime::memory::HostSlice;
-use icicle_runtime::memory::DeviceSlice;
 use icicle_bn254::curve::CurveCfg as Bn254CurveCfg;
-use icicle_core::curve::{Curve, Projective};
+use icicle_core::curve::{Curve, Projective, Affine};
 use icicle_bn254::curve::ScalarCfg;
 use icicle_core::traits::GenerateRandom;
+use icicle_bn254::curve::ScalarField as Bn254ScalarField;
+use icicle_runtime::memory::{DeviceVec, HostSlice};
 
 use cryptobazaar::utils::load_backend;
 use criterion::{criterion_group, criterion_main, Criterion};
@@ -14,8 +14,8 @@ use criterion::{criterion_group, criterion_main, Criterion};
 fn msm_host_benchmark() -> Vec<Projective::<Bn254CurveCfg>> {
     let cfg = MSMConfig::default();
     let mut projective_output = vec![Projective::<Bn254CurveCfg>::zero(); 1];
-    let scalars = ScalarCfg::generate_random(1000);
-    let bases = Bn254CurveCfg::generate_random_affine_points(1000);
+    let scalars = ScalarCfg::generate_random(10000);
+    let bases = Bn254CurveCfg::generate_random_affine_points(10000);
     
     unsafe{
         msm::msm(
@@ -32,21 +32,40 @@ fn msm_host_benchmark() -> Vec<Projective::<Bn254CurveCfg>> {
 
 fn msm_device_benchmark() -> Vec<Projective::<Bn254CurveCfg>> {
     let cfg = MSMConfig::default();
-    let mut projective_output = vec![Projective::<Bn254CurveCfg>::zero(); 1];
-    let scalars = ScalarCfg::generate_random(1000);
-    let bases = Bn254CurveCfg::generate_random_affine_points(1000);
+    let n = 1024usize;
+    let mut output_cpu = vec![Projective::<Bn254CurveCfg>::zero(); 1];
+    let scalars = ScalarCfg::generate_random(n);
+    let bases = Bn254CurveCfg::generate_random_affine_points(n);
+
+    let mut input_scalars_gpu =
+        DeviceVec::<Bn254ScalarField>::device_malloc(n).expect("Failed to allocate device memory for input");
+    let mut input_bases_gpu =
+        DeviceVec::<Affine::<Bn254CurveCfg>>::device_malloc(n).expect("Failed to allocate device memory for input");
+    let mut output_gpu =
+        DeviceVec::<Projective::<Bn254CurveCfg>>::device_malloc(n).expect("Failed to allocate device memory for output");
     
+    input_scalars_gpu
+        .copy_from_host(HostSlice::from_slice(&scalars))
+        .expect("Failed to copy data to GPU");
+    input_bases_gpu
+        .copy_from_host(HostSlice::from_slice(&bases))
+        .expect("Failed to copy data to GPU");
+
     unsafe{
         msm::msm(
-            DeviceSlice::from_slice(&scalars),
-            DeviceSlice::from_slice(&bases),
+            &input_scalars_gpu[..],
+            &input_bases_gpu[..],
             &cfg,
-            DeviceSlice::from_mut_slice(&mut projective_output),
+            &mut output_gpu[..],
         )
         .unwrap();
     }
 
-    projective_output
+    output_gpu
+        .copy_to_host(HostSlice::from_mut_slice(&mut output_cpu))
+        .expect("Failed to copy data back to CPU");
+
+    output_cpu
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
